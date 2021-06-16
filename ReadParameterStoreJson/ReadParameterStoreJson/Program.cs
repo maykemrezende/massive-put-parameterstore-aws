beecf7f954d6441d578f1b047582ff1171cb5922
Mayke Rezende <mayke.rezende@mark2market.onmicrosoft.com>
2021-06-16 15:06:20 -0300

    adjustments

--- ReadParameterStoreJson/ReadParameterStoreJson/Program.cs
+++ ReadParameterStoreJson/ReadParameterStoreJson/Program.cs
@@ -1,28 +1,45 @@
-﻿using Newtonsoft.Json;
+﻿using Amazon;
+using Amazon.Runtime;
+using Amazon.Runtime.CredentialManagement;
+using Amazon.SimpleSystemsManagement;
+using Amazon.SimpleSystemsManagement.Model;
+using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace ReadParameterStoreJson
 {
     public class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            var credential = new SessionAWSCredentials("accessKey", "secretAccess", "sessionToken");
+                            
+            var ssmClient = new AmazonSimpleSystemsManagementClient(credential);
+
             var path = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory())
                 .Parent
                 .Parent
-                .FullName, 
+                .FullName,
                 "parameters.json");
 
-            using var reader = new StreamReader(path);            
+            using var reader = new StreamReader(path);
             var json = reader.ReadToEnd();
             var parameters = JsonConvert.DeserializeObject<ParameterJsonRoot>(json);
-            
-            foreach(var parameter in parameters.Parameters)
+
+            var result = new Dictionary<string, string>();
+
+            foreach (var parameter in parameters.Parameters)
             {
-                Console.WriteLine($"aws ssm put-parameter --name {parameter.Name} --value {parameter.Value} --type String");
+                var inputParameter = new PutParameterRequest() { Name = parameter.Name, Value = parameter.Value, Type = ParameterType.String };
+                var putResponse = await ssmClient.PutParameterAsync(inputParameter);
+                result.Add(parameter.Name, putResponse.HttpStatusCode.ToString());
             }
 
+            Console.WriteLine($"Resultado: \n {result}");
             Console.ReadKey();
         }
     }
